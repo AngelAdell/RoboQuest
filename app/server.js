fa461305be2a219c85350cc1afcb7c59abdd0c46
Angel Adell <angeldeniseadell@gmail.com>
2024-04-22 17:57:38 -0400

    created gitignore file and main.js. refactored folders

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use('/users', require('./api/users.js'));
+app.use('/users', require('../api/users.js'));
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
